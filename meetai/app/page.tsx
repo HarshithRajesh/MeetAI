fe713012ae5c283fec496c84c2ed9626c4f28a64
harshith <harshithr2004@gmail.com>
2026-01-04 14:42:10 +0530

    better auth auth schema added 01

--- meetai/app/page.tsx
+++ meetai/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 
 import { Button } from "@/components/ui/button"
+import { useState } from "react";
 export default function Home() {
+  const [email, setEmail] = useState("")
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
   return (
-    <Button>
-      Click Me
-    </Button>
+    <div>
+      <Input></Input>
+
+    </div>
+
   )
+
 }
