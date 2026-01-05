5ff22be93557f5733223d48e18edeb2fcdcc6b88
harshith <harshithr2004@gmail.com>
2026-01-05 20:37:03 +0530

    better auth auth schema added 02

--- meetai/app/page.tsx
+++ meetai/app/page.tsx
@@ -1,17 +1,51 @@
 "use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { error } from "console";
 import { useState } from "react";
+
 export default function Home() {
-  const [email, setEmail] = useState("")
-  const [name, setName] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    },{
+      onError: (ctx) => {
+        window.alert(ctx.error.message);
+      },
+      onSuccess: () =>{
+        window.alert("Success")
+      }
+    });
+  }
+
+
   return (
-    <div>
-      <Input></Input>
-
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Create user</Button>
     </div>
-
-  )
-
+  );
 }
