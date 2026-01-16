7caa5e49f684aed3aa7d42c5b51e9b074a991a46
harshith <harshithr2004@gmail.com>
2026-01-16 23:12:00 +0530

    socials connected google and github 02

--- meetai/app/page.tsx
+++ meetai/app/page.tsx
@@ -1,7 +1,18 @@
 import { HomeView } from "@/modules/auth/ui/views/home/ui/views/home-view";
 
-const Page = () => {
+import { auth } from "@//lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const Page = async () => {  
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in"); 
+  }
   return <HomeView />;
 }
 
-// export default Page;
+export default Page;
